d43690c79e6dd1c70dba22003e069cfcc1ad6788
hariton <pitsikhariton@gmail.com>
2024-04-10 20:47:22 +0400

    Теперь функция fib(n) вычисляет значение, начиная с 0

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 
 int fib(int n) {
-    if (n <= 2) 
+    if (n == 0) 
+        return 0;
+    else if (n == 1)
         return 1;
-    else 
-        return fib(n-1) + fib(n-2);
+    else
+    {
+        int first = 0;
+        int second = 1;
+        int next = 1;
+        for (int i = 2; i < n; i++)
+        {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        return next;
+    } 
+        
 }
 
 int main()
