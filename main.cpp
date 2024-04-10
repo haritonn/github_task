98af14717499f88cff187ca6ccafb4845ae2d3ee
hariton <pitsikhariton@gmail.com>
2024-04-10 20:39:39 +0400

    Добавил рекурсивную функцию fib(n)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n) {
+    if (n <= 2) 
+        return 1;
+    else 
+        return fib(n-1) + fib(n-2);
+}
+
 int main()
 {
+    int n;
     cout << "Hello world!\n";
+    cout << "N = "; cin >> n;
+    cout << "fib(n) = " << fib(n) << endl;
     return 0;
 }
